d3ed0a723b812ee4c63d7c7b4158c8f927fff5c9
Ishank Chopra <ishankchopra@gmail.com>
2022-02-09 20:16:38 +0530

    Build Header Component

---
+++ components/Home/Header.js
@@ -0,0 +1,98 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Image } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { color } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/header-logo.png")}
+        />
+      </TouchableOpacity>
+      <View style={styles.iconsContainer}>
+        <TouchableOpacity>
+          <Image
+            source={{
+              uri: "https://img.icons8.com/fluency-systems-regular/60/ffffff/plus-2-math.png",
+            }}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Image
+            source={{
+              uri: "https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png",
+            }}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <View style={styles.unreadBadge}>
+            <Text>
+              <Text style={styles.unreadBadgeText}>11</Text>
+            </Text>
+          </View>
+          <Image
+            source={{
+              uri: "https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png",
+            }}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 20,
+  },
+  iconsContainer: {
+    flexDirection: "row",
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
+    resizeMode: "contain",
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
+    resizeMode: "contain",
+  },
+  unreadBadge: {
+    backgroundColor: "#FF3250",
+    position: "absolute",
+    left: 20,
+    bottom: 18,
+    width: 25,
+    height: 18,
+    borderRadius: 25,
+    alignContent: "center",
+    justifyContent: "center",
+    zIndex: 100,
+  },
+  unreadBadgeText: {
+    color: "white",
+    fontWeight: "600",
+  },
+  logo: {
+    width: 100,
+    height: 50,
+    resizeMode: "contain",
+  },
+});
