9f23c3041885569e221deb9902e6b0d11ba0434b
Ishank Chopra <ishankchopra@gmail.com>
2022-02-09 23:47:49 +0530

    Build Stories Component

---
+++ data/users.js
@@ -0,0 +1,33 @@
+export const USERS = [
+  {
+    user: "Dani Daniels",
+    image:
+      "https://pbs.twimg.com/profile_images/939493257515544577/QjofCtLh_400x400.jpg",
+  },
+  {
+    user: "Elon Musk",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/220px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
+  },
+
+  {
+    user: "Angelina Jolie",
+    image:
+      "https://domf5oio6qrcr.cloudfront.net/medialibrary/8370/Angelina-Jolie.jpg",
+  },
+  {
+    user: "Satya Nadella",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/MS-Exec-Nadella-Satya-2017-08-31-22_%28cropped%29.jpg/220px-MS-Exec-Nadella-Satya-2017-08-31-22_%28cropped%29.jpg",
+  },
+  {
+    user: "Kiara Advani",
+    image:
+      "https://filmfare.wwmindia.com/content/2020/dec/kiaraadvani21606907531.jpg",
+  },
+  {
+    user: "Cristiano Ronaldo",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNDZhNTc1Y2MtZjA2NC00MDM5LThiODMtNTVhZTMxNmNmYWIwXkEyXkFqcGdeQXVyMTExNDQ2MTI@._V1_UY1200_CR129,0,630,1200_AL_.jpg",
+  },
+];
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,11 +2,13 @@ import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import Header from "../components/Home/Header";
+import Stories from "../components/Home/Stories";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <Stories />
     </SafeAreaView>
   );
 };
