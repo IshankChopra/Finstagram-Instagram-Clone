8c8a48ad34914844e8a74ee0190dcc43c89efaea
Ishank Chopra <ishankchopra@gmail.com>
2022-02-11 17:00:50 +0530

    Build PostHeader

---
+++ data/posts.js
@@ -0,0 +1,36 @@
+import { USERS } from "./users";
+
+export const POSTS = [
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    user: USERS[0].user,
+    likes: 123,
+    caption: "CAT",
+    profile_picture: USERS[0].image,
+    comments: [
+      {
+        user: "ishank",
+        comment: "what a cat",
+      },
+      {
+        user: "chopra",
+        caption: "lovely cat",
+      },
+    ],
+  },
+  {
+    imageUrl:
+      "https://lh3.googleusercontent.com/ogw/ADea4I7J3oe3ftaaRZNrrMpC9RJfSLpyVzjIRRptYbjK=s83-c-mo",
+    user: USERS[1].user,
+    likes: 456,
+    caption: "PUNEET CHOPRA",
+    profile_picture: USERS[1].image,
+    comments: [
+      {
+        user: "kjr",
+        comment: "jfh jfhe jfh",
+      },
+    ],
+  },
+];
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,12 +3,20 @@ import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import Header from "../components/Home/Header";
 import Stories from "../components/Home/Stories";
+import { POSTS } from "../data/posts";
+import { ScrollView } from "react-native";
+import Post from "../components/Home/Post";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
       <Stories />
+      <ScrollView>
+        {POSTS.map((post, index) => (
+          <Post post={post} key={index} />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
