fd89f7b487dea49c134f2cb031802339408196a6
Ishank Chopra <ishankchopra@gmail.com>
2022-02-22 22:45:48 +0530

    Final Commit

--- App.js
+++ App.js
@@ -4,3 +4,11 @@ import AuthNavigation from "./AuthNavigation";
 export default function App() {
   return <AuthNavigation />;
 }
+
+import { LogBox } from "react-native";
+
+// Ignore log notification by message:
+LogBox.ignoreLogs(["Warning: ..."]);
+
+// Ignore all log notifications:
+LogBox.ignoreAllLogs();
